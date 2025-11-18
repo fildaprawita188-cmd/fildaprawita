4a805442dfc5b79848c6d42288a4826bc0f3ea8e
fildaprawita188-cmd <fildaprawita188@gmail.com>
2025-11-18 09:08:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+        id: 1,
+        fullName: "Filda Prawita",
+        phone: 6289624102638,
+        email: "fildaprawita188@gmail.com",
+        location: "Sambas",
+    },
+    {
+        id: 2,
+        fullName: "Mochamad Irvan",
+        phone: 62881080070700,
+        email: "mchdirvan@example.com",
+        location: "Jakarta",
+    },
+    {
+        id: 3,
+        fullName: "Adhitya Sofyan",
+        phone: 62881080080800,
+        email: "adhitya@exsample.com",
+        location: "Bandung",
+    },
+];
+
+console.log(contacts);
